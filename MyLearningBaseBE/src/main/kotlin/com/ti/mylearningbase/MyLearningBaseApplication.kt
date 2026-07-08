60c7a9d8369a96e844a7928bf06e591539be9202
Marcin <marcinpokusinski@gmail.com>
2026-07-09 00:50:15 +0200

    create backend for project

---
+++ MyLearningBaseBE/src/main/kotlin/com/ti/mylearningbase/MyLearningBaseApplication.kt
@@ -0,0 +1,11 @@
+package com.ti.mylearningbase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyLearningBaseApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyLearningBaseApplication>(*args)
+}
---
+++ MyLearningBaseBE/src/test/kotlin/com/ti/mylearningbase/MyLearningBaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ti.mylearningbase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyLearningBaseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
